b89e9dd777bb76109e2a2c1b87acad0cf786aec9
thluiz <contato@thluiz.com>
2018-03-23 06:02:16 -0300

    initial version

---
+++ server.ts
@@ -0,0 +1,18 @@
+require('dotenv').load();
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var pub = __dirname + '/public';
+var app = express();
+
+app.use(express.static('public'))
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+var Recaptcha = require('express-recaptcha');
+var recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SITE_SECRET);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 3000, () => console.log('Voucher app listening on port 3000!'))
