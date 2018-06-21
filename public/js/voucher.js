4e60612c09599cdeaa015e2f14b1382213abb282
thluiz <contato@thluiz.com>
2018-06-21 05:59:26 -0300

    external branches support

--- public/js/voucher.js
+++ public/js/voucher.js
@@ -1,24 +1,28 @@
 let data = "";
 
 function loadViewData(dt) {
-    data = dt;    
+    data = dt;
 }
 
 function changeBranch(id) {
     var select = $('#inputSchedule');
-    var options = ['<option>Selecione o horário preferencial</option>'];                
+    var lblExternal = $('#lblExternal');
+    var template = $.templates('#externalBranchMessageTemplate');
+    var scheduleDiv = $('#schedule');
+
+    var options = ['<option>Selecione o horário preferencial</option>'];
     select
     .find('option')
     .remove()
     .end()
 
     var branch = $.grep(data.branches, function(b) { return b.id == id })[0];
-    
+
     if(branch.voucher_map) {
         $.each(branch.voucher_map, function(i, m) {
             var description = '';
             description += m.title ? m.title + ' - ' : '';
-                                       
+
             description += formatArray($.map(m.week_days, (val) => val.name));
 
             description += " de ";
@@ -30,11 +34,25 @@ function changeBranch(id) {
         });
 
         $.each(options, function(i, v) {
-        select.append($(v));
-        }); 
+            select.append($(v));
+        });
+        lblExternal.hide();
+        scheduleDiv.show();
     } else {
-        select.append('<option value="-1">Em breve! Desejo ser avisado quando estiver disponível.</option>');
-    }       
+        if(branch.category_id != 3) {
+            select.append('<option value="-1">Em breve! Desejo ser avisado quando estiver disponível.</option>');
+            lblExternal.hide();
+            scheduleDiv.show();
+        } else {
+            lblExternal.html(template.render({
+                contact_phone: branch.contact_phone,
+                contact_email: branch.contact_email
+            }));
+
+            lblExternal.show();
+            scheduleDiv.hide();
+        }
+    }
 }
 
 function formatArray(arr){
@@ -53,7 +71,7 @@ function formatArray(arr){
     return outStr;
 }
 
-function sendData() {            
+function sendData() {
     var nameField = $('#inputName');
     var emailField = $('#inputEmail');
     var cpfField = $('#inputCPF');
@@ -65,7 +83,7 @@ function sendData() {
     var additionalAnswerField = $('#inputAdditionalAnswer');
     var questionField = $('#inputQuestion');
     var inviteKeyField = $('#inputInviteKey');
-    
+
     var data = {
         name: nameField.val(),
         email: emailField.val(),
@@ -74,9 +92,9 @@ function sendData() {
         socialLinks: socialLinksField.val(),
         unit: unitField.val(),
         schedule: $('#inputSchedule').val(),
-        voucher_id: voucherField.val(), 
-        invite: inviteKeyField.length > 0 ? inviteKeyField.val() : null,  
-        "g-recaptcha-response": $('#g-recaptcha-response').val() 
+        voucher_id: voucherField.val(),
+        invite: inviteKeyField.length > 0 ? inviteKeyField.val() : null,
+        "g-recaptcha-response": $('#g-recaptcha-response').val()
     };
 
     if(additionalAnswerField.length > 0) {
@@ -100,44 +118,44 @@ function sendData() {
             var errors = [];
 
             if(!data) {
-                errors[errors.length] = "Ocorreu um erro ao enviar seus dados, por favor, tente novamente";                    
+                errors[errors.length] = "Ocorreu um erro ao enviar seus dados, por favor, tente novamente";
             }
 
             if(data && !data.success) {
                 errors = data.errors;
             }
 
-            if(errors.length > 0) {                    
+            if(errors.length > 0) {
                 $.each(errors, function(i, e) {
                     if(e === "invalid-input-response") {
                         e = "Por favor, responda ao captcha";
-                    } else if(e === "timeout-or-duplicate") {                        
+                    } else if(e === "timeout-or-duplicate") {
                         e = "Por favor, Responda ao reCaptcha novamente"
                     }
                     $('#ErrorContainer').append(tmpl.render({message: e}));
-                });                
+                });
 
                 grecaptcha.reset();
-                
+
                 $('#ErrorContainer').show('fade');
                 $('#sendingButton').hide('fade', function() {
-                    $('#submitField').show('fade');                    
-                });    
+                    $('#submitField').show('fade');
+                });
 
                 $('#sendingButtonMobile').hide('fade', function() {
-                    $('#submitFieldMobile').show('fade');                    
-                });    
+                    $('#submitFieldMobile').show('fade');
+                });
                 return;
             }
-            
-            $('#sendingButton').hide('fade', function() {                
-                location.href = "/voucher_final" + (voucherField.val() > 0 ? "/" + voucherField.val() : ""); 
-            });                
+
+            $('#sendingButton').hide('fade', function() {
+                location.href = "/voucher_final" + (voucherField.val() > 0 ? "/" + voucherField.val() : "");
+            });
         }
-    });    
+    });
 }
 
-$(function() {    
+$(function() {
     $('#submitField').click(sendData);
     $('#submitFieldMobile').click(sendData);
     if($('#inputUnit').val() > 0) {
@@ -148,33 +166,33 @@ $(function() {
 
     // Find all YouTube videos
     var $allVideos = $("iframe"),
-    
+
         // The element that is fluid width
         $fluidEl = $("body");
-    
+
     console.log($allVideos);
 
     // Figure out and save aspect ratio for each video
-    $allVideos.each(function() {    
+    $allVideos.each(function() {
       $(this).data('aspectRatio', 315 / 560)
     });
-    
+
     // When the window is resized
-    $(window).resize(function() {    
-    
+    $(window).resize(function() {
+
       // Resize all videos according to their own aspect ratio
       $allVideos.each(function() {
         var $el = $(this);
 
         var newWidth = $($el.parent()).width();
         console.log(newWidth);
-        
+
         $el
           .width(newWidth)
           .height(newWidth * $el.data('aspectRatio'));
-    
+
       });
-    
+
     // Kick off one resize to fix all videos on page load
     }).resize();
 
